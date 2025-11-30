dcb285ad30ac169de5812d64b0b5e6ca0c43fcfe
Ravshan <rawshanraxmonberdiyev0@gmail.com>
2025-11-30 17:40:19 +0100

    Fixing bugs

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
 import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare, User } from "lucide-react";
 import { Link } from "react-router-dom";
-import Warning from "../components/Warning";
 
 import AuthImagePattern from "../components/AuthImagePattern";
 import Warning from "../components/Warning";
